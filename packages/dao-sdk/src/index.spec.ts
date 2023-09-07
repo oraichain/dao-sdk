47ae10f8a4f0103f433c5de4ec48cfaadddfc354
Pham Tu <tubackkhoa@gmail.com>
2023-09-07 11:36:46 +0700

    update test

--- packages/dao-sdk/src/index.spec.ts
+++ packages/dao-sdk/src/index.spec.ts
@@ -1,10 +1,9 @@
 import { coin } from '@cosmjs/amino';
 import { toBinary } from '@cosmjs/cosmwasm-stargate';
 import * as commonArtifacts from '@oraichain/common-contracts-build';
-import { Cw20BaseTypes } from '@oraichain/common-contracts-sdk';
 import { SimulateCosmWasmClient } from '@oraichain/cw-simulate';
 import * as daoArtifacts from '@oraichain/dao-contracts-build';
-import { DaoDaoCoreClient, DaoDaoCoreTypes } from '@oraichain/dao-contracts-sdk';
+import { DaoDaoCoreClient, DaoDaoCoreTypes, DaoProposalSingleTypes, DaoVotingCw20StakedClient, DaoVotingCw20StakedTypes } from '@oraichain/dao-contracts-sdk';
 import fs from 'fs';
 
 const client = new SimulateCosmWasmClient({
@@ -19,14 +18,46 @@ describe('simple_case', () => {
   let daoContract: DaoDaoCoreClient;
 
   beforeEach(async () => {
-    const cw20InitMsg: Cw20BaseTypes.InstantiateMsg = {
-      name: 'DAO',
-      symbol: 'DAO',
-      decimals: 6,
-      initial_balances: []
-    };
     const { codeId: cw20Id } = await client.upload(senderAddress, fs.readFileSync(commonArtifacts.getContractDir('cw20-base')), 'auto');
-    const { contractAddress } = await daoArtifacts.deployContract<DaoDaoCoreTypes.InstantiateMsg>(
+    const { codeId: voteModId } = await client.upload(senderAddress, fs.readFileSync(daoArtifacts.getContractDir('dao-voting-cw20-staked')), 'auto');
+    const { codeId: cw20StakeId } = await client.upload(senderAddress, fs.readFileSync(daoArtifacts.getContractDir('cw20-stake')), 'auto');
+
+    const { codeId: proposalSingleId } = await client.upload(senderAddress, fs.readFileSync(daoArtifacts.getContractDir('dao-proposal-single')), 'auto');
+    const voteModInitMsg: DaoVotingCw20StakedTypes.InstantiateMsg = {
+      token_info: {
+        new: {
+          code_id: cw20Id,
+          label: 'DAO DAO governance token',
+          name: 'DAO',
+          symbol: 'DAO',
+          decimals: 6,
+          initial_balances: [{ amount: '100000000', address: senderAddress }],
+          staking_code_id: cw20StakeId
+        }
+      },
+      active_threshold: {
+        absolute_count: {
+          count: '100'
+        }
+      }
+    };
+
+    const proposalSingleInitMsg: DaoProposalSingleTypes.InstantiateMsg = {
+      threshold: {
+        threshold_quorum: {
+          quorum: { percent: '0.015' },
+          threshold: { majority: {} }
+        }
+      },
+      max_voting_period: { time: 604800 }, // One week.
+      only_members_execute: true,
+      allow_revoting: false,
+      pre_propose_info: {
+        anyone_may_propose: {}
+      },
+      close_proposal_on_execution_failure: true
+    };
+    const { contractAddress: coreAddress } = await daoArtifacts.deployContract<DaoDaoCoreTypes.InstantiateMsg>(
       client,
       senderAddress,
       {
@@ -35,33 +66,30 @@ describe('simple_case', () => {
         automatically_add_cw20s: true,
         automatically_add_cw721s: true,
         voting_module_instantiate_info: {
-          code_id: cw20Id,
-          msg: toBinary(cw20InitMsg),
+          code_id: voteModId,
+          msg: toBinary(voteModInitMsg),
           admin: { core_module: {} },
           label: 'voting module'
         },
         proposal_modules_instantiate_info: [
           {
-            code_id: cw20Id,
-            msg: toBinary(cw20InitMsg),
+            code_id: proposalSingleId,
+            msg: toBinary(proposalSingleInitMsg),
             admin: { core_module: {} },
-            label: 'governance module 0'
+            label: 'governance module'
           }
         ]
       },
       'dao-dao-core'
     );
     client.app.bank.setBalance(senderAddress, [coin('100000000', 'orai'), coin('100000000', 'usdt')]);
-    daoContract = new DaoDaoCoreClient(client, senderAddress, contractAddress);
+    daoContract = new DaoDaoCoreClient(client, senderAddress, coreAddress);
   });
 
-  it('test-state', async () => {
-    const { config } = await daoContract.dumpState();
-    expect(config).toMatchObject({
-      name: 'DAO DAO',
-      description: 'A DAO that builds DAOs.',
-      automatically_add_cw20s: true,
-      automatically_add_cw721s: true
-    });
+  it('test-init', async () => {
+    const votingAddress = await daoContract.votingModule();
+    const votingModuleContract = new DaoVotingCw20StakedClient(client, senderAddress, votingAddress);
+    const govTokenAddress = await votingModuleContract.tokenContract();
+    console.log('govTokenAddress', govTokenAddress);
   });
 });

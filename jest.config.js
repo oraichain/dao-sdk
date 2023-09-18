052389242a5d9ba1046fd3e9f142620d5ecd9e02
Pham Tu <tubackkhoa@gmail.com>
2023-09-18 17:51:08 +0700

    use es2020

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.ts?$': ['esbuild-jest', { isolatedModules: true }]
+    '^.+\\.ts?$': ['esbuild-jest', { isolatedModules: true, target: 'ES2020' }]
   },
   testPathIgnorePatterns: ['build/', 'node_modules/']
 };
